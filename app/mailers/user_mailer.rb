db77153a8edfce2cfebb31d39c892b43016288ac
nfsantibanez <nfsantibanez@uc.cl>
2018-02-06 16:35:12 -0300

    fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,31 +1,29 @@
 class UserMailer < ApplicationMailer
 
   def supervisor_email(user, request)
-    puts('entrando a super_mailer')
+    puts(5*'entrando a super_mailer')
     @n_link = request.link
     @supervisor = user.supervisor
-    @to = 'nisanti@ccu.cl '# user.supervisor_email
+    @to = 'solcelulares@ccu.cl '# user.supervisor_email
     @subject = request.name+" a creado la solicitud n° "+request.n_request+" que necesita su aprobación."
     @body = request.email_sended+"."
-
     mail(to: @to, subject: @subject)
   end
 
   def admin_email(user, request)
-    puts('entrando a admin_mailer')
+    puts(5*'entrando a admin_mailer')
     @user = user
     @request = request
-    @to = 'nisanti@ccu.cl'
+    @to = 'solcelulares@ccu.cl'
     @subject = request.name+" a creado la solicitud n° "+request.n_request+" que necesita su aprobación."
     @body = request.email_sended+"."
-
     mail(to: @to, subject: @subject)
   end
 
   def user_email(user, request)
-    puts('entrando a user_mailer')
+    puts(5*'entrando a user_mailer')
     @user = user
-    @to = 'nisanti@ccu.cl' # user.email
+    @to = 'solcelulares@ccu.cl' # user.email
     @subject = "Se ha creado la Solicitud n° "+request.n_request
     @body = "Usted a creado la solicitud n° "+request.n_request
 
@@ -33,11 +31,11 @@ class UserMailer < ApplicationMailer
   end
 
   def change_email(user, request, change)
-    puts('entrando a change_mailer')
+    puts(5*'entrando a change_mailer')
     @user = user
     @request = request
     @change = change
-    @to = 'nisanti@ccu.cl' # user.email
+    @to = 'solcelulares@ccu.cl' # user.email
     @subject = "Cambio de estado de Solicitud n° "+request.n_request
     @body = "La solicitud n° "+request.n_request+" ha cambiado de estado a "
 
